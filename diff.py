23f90cf48342ac5f9c13faaf90e839d8945a1b75
Breyten Ernsting <bje@dds.nl>
2018-09-01 14:37:42 +0200

    Read GML

--- diff.py
+++ diff.py
@@ -9,15 +9,52 @@ import json
 from pprint import pprint
 import json
 
+from lxml import etree
+import shapely
+from shapely.geometry import asShape, Polygon, LinearRing
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def get_gml_shapes(gml_file):
+    ns = {
+        'kad': "http://www.kadaster.nl/kad/pdok",
+        'gml': 'http://www.opengis.net/gml/3.2'
+    }
+    xml = etree.parse(gml_file)
+    shapes = []
+    for gem in xml.xpath('//kad:Gemeenten', namespaces=ns):
+        info = {
+            'code': 'GM' + ''.join(
+                gem.xpath('./kad:Code/text()', namespaces=ns)),
+            'naam': ''.join(
+                gem.xpath('./kad:Gemeentenaam/text()', namespaces=ns))
+        }
+        points_txt = ''.join(
+            gem.xpath('.//gml:LinearRing//text()', namespaces=ns))
+        points = [
+            float(x) for x in
+            points_txt.split()]
+        shape = asShape(Polygon(LinearRing(
+            chunks(points, 2)
+        )))
+        shapes.append((info, shape))
+    return shapes
+
 
 def compare(older_year, newer_year, older, newer):
     print("Comparing %s vs %s" % (older_year, newer_year,))
     older_keys = {r['Key'].strip() for r in older['value']}
     newer_keys = {r['Key'].strip() for r in newer['value']}
-    print("Disappeared:")
-    print(older_keys - newer_keys)
-    print("New:")
-    print(newer_keys - older_keys)
+    disappeared = older_keys - newer_keys
+    added = newer_keys - older_keys
+    return (
+        [o for o in older['value'] if o['Key'].strip() in disappeared],
+        [n for n in newer['value'] if n['Key'].strip() in added],)
 
 
 def main(argv):
@@ -29,7 +66,20 @@ def main(argv):
 
     for year in sorted(yearly.keys()):
         if (year - 1) in yearly:
-            compare(year - 1, year, yearly[year - 1], yearly[year])
+            gone, created = compare(
+                year - 1, year, yearly[year - 1], yearly[year])
+            print("Disappeared:")
+            pprint(gone)
+            print("New:")
+            pprint(created)
+            old_shapes = []
+            new_shapes = []
+            gg_path = 'grenzen/%s/Gemeentegrenzen.gml' % (year,)
+            if os.path.exists(gg_path):
+                new_shapes = get_gml_shapes(gg_path)
+            gg_path = 'grenzen/%s/Gemeentegrenzen.gml' % (year - 1,)
+            if os.path.exists(gg_path):
+                old_shapes = get_gml_shapes(gg_path)
     return 0
 
 if __name__ == '__main__':
